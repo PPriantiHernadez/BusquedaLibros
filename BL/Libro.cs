7b967627b64df84768a6ad542f6b7d96b3b1b07c
PPriantiHernandez <ppriantihernandez@gmailc.om>
2023-05-23 15:28:46 -0600

    ML result

--- BL/Libro.cs
+++ BL/Libro.cs
@@ -8,5 +8,35 @@ namespace BL
 {
     public class Libro
     {
+        public static ML.Result Add(ML.Aseguradora aseguradora)
+        {
+            ML.Result result = new ML.Result();
+
+            try
+            {
+                using (DL.PpriantiProgramacionNcapasCoreContext context = new DL.PpriantiProgramacionNcapasCoreContext())
+                {
+                    int queryEF = context.Database.ExecuteSqlRaw($"AseguradoraAdd '{aseguradora.Nombre}', {aseguradora.Usuario.IdUsuario}");
+                    //int queryEF = context.Database.ExecuteSqlRaw($"SemestreAdd '{semestre.Nombre}', {semestre.IdSemestre} ");
+
+
+                    if (queryEF > 0)
+                    {
+                        result.Correct = true;
+                    }
+                    else
+                    {
+                        result.Correct = false;
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = "Ocurrio un error al insertar la aseguradora" + ex;
+            }
+            return result;
+        }
     }
 }
