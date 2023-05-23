4d150064108eba3b01d55e406265054cbd46da09
PPriantiHernandez <ppriantihernandez@gmailc.om>
2023-05-23 15:02:22 -0600

    ML

---
+++ ML/Autor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class Autor
+    {
+        public byte IdAutor { get; set; } //PK
+        public string Nombre { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public string FechaNacimiento { get; set; }
+        public string PaisOrigen { get; set; }
+
+        public List<object> Autores { get; set; }
+
+    }
+}
---
+++ ML/Libro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class Libro
+    {
+        public byte IdLibro { get; set; } //PK
+        public string TituloLibro { get; set; }
+        public string FechaPublicacion { get; set; }
+        public string Sipnosis { get; set; }
+        public string Portada { get; set; }
+
+        public ML.Autor Autor { get; set; }//FK
+        public ML.Editorial Editorial { get; set; }//FK
+
+        public List<object> Libros { get; set; }
+    }
+}
