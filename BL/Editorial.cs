3c01d6cced62463e76e8ceb792679036e3724cc1
PPriantiHernandez <ppriantihernandez@gmailc.om>
2023-05-23 16:29:04 -0600

    BL

--- BL/Editorial.cs
+++ BL/Editorial.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,39 @@ namespace BL
 {
     public class Editorial
     {
+        public static ML.Result GetAll()
+        {
+            ML.Result result = new ML.Result();
+
+            try
+            {
+                using (DL.SistemaBusquedaContext context = new DL.SistemaBusquedaContext())
+                {
+                    var editorialList = context.Autors.FromSqlRaw($"EditorialGetAll").ToList();
+
+                    result.Objects = new List<object>();
+
+                    foreach (var row in editorialList)
+                    {
+                        ML.Editorial editorial = new ML.Editorial();
+
+                        editorial.IdEditorial = row.IdEditorial;
+                        editorial.Nombre = row.Nombre;
+
+                        result.Objects.Add(editorial);
+
+                    }
+                    result.Correct = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.Ex = ex;
+                result.ErrorMessage = ex.Message;
+
+            }
+            return result;
+        }
     }
 }
--- BL/Libro.cs
+++ BL/Libro.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,17 +9,17 @@ namespace BL
 {
     public class Libro
     {
-        public static ML.Result Add(ML.Aseguradora aseguradora)
+
+        //CREAR NUEVA INFORMACION DE LIBRO :) PRISCILA
+        public static ML.Result Add(ML.Libro libro)
         {
             ML.Result result = new ML.Result();
 
             try
             {
-                using (DL.PpriantiProgramacionNcapasCoreContext context = new DL.PpriantiProgramacionNcapasCoreContext())
+                using (DL.SistemaBusquedaContext context = new DL.SistemaBusquedaContext())
                 {
-                    int queryEF = context.Database.ExecuteSqlRaw($"AseguradoraAdd '{aseguradora.Nombre}', {aseguradora.Usuario.IdUsuario}");
-                    //int queryEF = context.Database.ExecuteSqlRaw($"SemestreAdd '{semestre.Nombre}', {semestre.IdSemestre} ");
-
+                    int queryEF = context.Database.ExecuteSqlRaw($"LibroAdd '{libro.TituloLibro}', '{libro.FechaPublicacion}' ,{libro.Autor.IdAutor}, {libro.Editorial.IdEditorial}, '{libro.Sipnosis}','{libro.Portada}'");
 
                     if (queryEF > 0)
                     {
@@ -29,14 +30,154 @@ namespace BL
                         result.Correct = false;
                     }
                 }
-
             }
             catch (Exception ex)
             {
                 result.Correct = false;
-                result.ErrorMessage = "Ocurrio un error al insertar la aseguradora" + ex;
+                result.ErrorMessage = "Ocurrio un error al insertar el libro" + ex;
             }
             return result;
         }
+
+        //CREAR CONSULTAS POR AUTOR :) PRISCILA
+
+        public static ML.Result LibroGetbyAutor(byte IdAutor)
+        {
+            ML.Result result = new ML.Result();
+
+            try
+            {
+                using (DL.SistemaBusquedaContext context = new DL.SistemaBusquedaContext())
+                {
+                    var libroList = context.Libros.FromSqlRaw($"LibroGetbyAutor  {IdAutor}").ToList();
+
+                    result.Objects = new List<object>();
+
+                    foreach (var row in libroList)
+                    {
+                        ML.Libro libro = new ML.Libro();
+
+                        libro.IdLibro = row.IdLibro;
+                        libro.TituloLibro = row.TituloLibro;
+                        libro.FechaPublicacion = row.FechaPublicacion.ToString("ddMMyyyy");
+
+                        libro.Autor = new ML.Autor();
+                        libro.Autor.IdAutor = row.IdAutor.Value;
+
+                        libro.Editorial = new ML.Editorial();
+                        libro.Editorial.IdEditorial = row.IdEditorial.Value;
+
+                        libro.Sipnosis = row.Sipnosis;
+                        libro.Portada = row.Portada;
+
+                       result.Objects.Add(libro);
+
+                    }
+                    result.Correct = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.Ex = ex;
+                result.ErrorMessage = ex.Message;
+
+            }
+            return result;
+        }
+
+        //CREAR CONSULTAS POR TITULO DE LIBRO :) PRISCILA
+
+        public static ML.Result LibroGetbyTitulo(string TituloLibro)
+        {
+            ML.Result result = new ML.Result();
+
+            try
+            {
+                using (DL.SistemaBusquedaContext context = new DL.SistemaBusquedaContext())
+                {
+                    var libroList = context.Libros.FromSqlRaw($"LibroGetbyTitulo  '{TituloLibro}'").ToList();
+
+                    result.Objects = new List<object>();
+
+                    foreach (var row in libroList)
+                    {
+                        ML.Libro libro = new ML.Libro();
+
+                        libro.IdLibro = row.IdLibro;
+                        libro.TituloLibro = row.TituloLibro;
+                        libro.FechaPublicacion = row.FechaPublicacion.ToString("ddMMyyyy");
+
+                        libro.Autor = new ML.Autor();
+                        libro.Autor.IdAutor = row.IdAutor.Value;
+
+                        libro.Editorial = new ML.Editorial();
+                        libro.Editorial.IdEditorial = row.IdEditorial.Value;
+
+                        libro.Sipnosis = row.Sipnosis;
+                        libro.Portada = row.Portada;
+
+                        result.Objects.Add(libro);
+
+                    }
+                    result.Correct = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.Ex = ex;
+                result.ErrorMessage = ex.Message;
+
+            }
+            return result;
+        }
+
+        //CREAR CONSULTAS POR FECHA DE PUBLICACION :) PRISCILA
+        public static ML.Result LibroGetbyFechaPublicacion(string FechaPublicacion)
+        {
+            ML.Result result = new ML.Result();
+
+            try
+            {
+                using (DL.SistemaBusquedaContext context = new DL.SistemaBusquedaContext())
+                {
+                    var libroList = context.Libros.FromSqlRaw($"LibroGetbyFechaPublicacion  '{FechaPublicacion}'").ToList();
+
+                    result.Objects = new List<object>();
+
+                    foreach (var row in libroList)
+                    {
+                        ML.Libro libro = new ML.Libro();
+
+                        libro.IdLibro = row.IdLibro;
+                        libro.TituloLibro = row.TituloLibro;
+                        libro.FechaPublicacion = row.FechaPublicacion.ToString("ddMMyyyy");
+
+                        libro.Autor = new ML.Autor();
+                        libro.Autor.IdAutor = row.IdAutor.Value;
+
+                        libro.Editorial = new ML.Editorial();
+                        libro.Editorial.IdEditorial = row.IdEditorial.Value;
+
+                        libro.Sipnosis = row.Sipnosis;
+                        libro.Portada = row.Portada;
+
+                        result.Objects.Add(libro);
+
+                    }
+                    result.Correct = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                result.Correct = false;
+                result.Ex = ex;
+                result.ErrorMessage = ex.Message;
+
+            }
+            return result;
+        }
+
     }
 }
