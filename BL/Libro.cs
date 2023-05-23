06af0696a619bc7318833a1af5b9216da2dc565c
HugoCardenasGon <HugoCardenasGon@gmail.com>
2023-05-23 16:26:23 -0600

    BL

--- BL/Libro.cs
+++ BL/Libro.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,35 +9,5 @@ namespace BL
 {
     public class Libro
     {
-        public static ML.Result Add(ML.Aseguradora aseguradora)
-        {
-            ML.Result result = new ML.Result();
-
-            try
-            {
-                using (DL.PpriantiProgramacionNcapasCoreContext context = new DL.PpriantiProgramacionNcapasCoreContext())
-                {
-                    int queryEF = context.Database.ExecuteSqlRaw($"AseguradoraAdd '{aseguradora.Nombre}', {aseguradora.Usuario.IdUsuario}");
-                    //int queryEF = context.Database.ExecuteSqlRaw($"SemestreAdd '{semestre.Nombre}', {semestre.IdSemestre} ");
-
-
-                    if (queryEF > 0)
-                    {
-                        result.Correct = true;
-                    }
-                    else
-                    {
-                        result.Correct = false;
-                    }
-                }
-
-            }
-            catch (Exception ex)
-            {
-                result.Correct = false;
-                result.ErrorMessage = "Ocurrio un error al insertar la aseguradora" + ex;
-            }
-            return result;
-        }
     }
 }
