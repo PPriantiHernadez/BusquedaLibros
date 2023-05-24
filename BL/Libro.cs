40824e8f8a47df6830c442c0358e9f1c914aa683
PPriantiHernandez <ppriantihernandez@gmailc.om>
2023-05-24 10:49:20 -0600

    cambio2405

--- BL/Libro.cs
+++ BL/Libro.cs
@@ -12,7 +12,7 @@ namespace BL
 
         //CREAR NUEVA INFORMACION DE LIBRO :) PRISCILA
         public static ML.Result Add(ML.Libro libro)
-        {
+        { 
             ML.Result result = new ML.Result();
 
             try
